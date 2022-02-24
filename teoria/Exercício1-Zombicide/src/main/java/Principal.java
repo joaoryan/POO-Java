997c33ffab3ed448a4f090b3cf1d2dbb1f8b796b
joaoryan <sjoaoryan@gmail.com>
2022-02-24 20:10:24 -0300

    feat: adicionando exercicio zombicide

---
+++ teoria/Exercício1-Zombicide/src/main/java/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        int rodada1 = 130;
+        int rodada2 = 240;
+        int rodada3 = 320;
+        double total;
+        double media;
+        total = rodada1 + rodada2 + rodada3;
+        media = total / 3;
+        System.out.println("O total de zumbis mortos é: ");
+        System.out.println(total);
+        System.out.println("A media de zumbis mortos é: ");
+        System.out.println(media);
+    }
+}
