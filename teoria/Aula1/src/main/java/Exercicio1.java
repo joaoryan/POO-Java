bc4fb19584810fbc176e31cca0200f34ed04c430
joaoryan <sjoaoryan@gmail.com>
2022-02-25 22:10:13 -0300

    feat: adicionando exercicio1

---
+++ teoria/Aula1/src/main/java/Exercicio1.java
@@ -0,0 +1,11 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        int numZumbi1 = 120;
+        int numZumbi2 = 120;
+        int numZumbi3 = 120;
+        int total = numZumbi1 + numZumbi2 + numZumbi3;
+
+        System.out.println(total);
+        System.out.println(total/3);
+    }
+}
