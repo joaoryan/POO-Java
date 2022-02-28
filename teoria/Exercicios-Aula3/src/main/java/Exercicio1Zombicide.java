3d6b6ad88877b22abb7627ca1667b1870b66890a
joaoryan <sjoaoryan@gmail.com>
2022-02-28 08:34:32 -0300

    Adicionando pasta e exercicio 1

---
+++ teoria/Exercicios-Aula3/src/main/java/Exercicio1Zombicide.java
@@ -0,0 +1,11 @@
+public class Exercicio1Zombicide {
+    public static void main(String[] args) {
+        int numZumbi1 = 120;
+        int numZumbi2 = 120;
+        int numZumbi3 = 120;
+        int total = numZumbi1 + numZumbi2 + numZumbi3;
+
+        System.out.println("O total de zumbis é " + total);
+        System.out.println("A media de zumbis mortos nas 3 rodadas é " + total / 3);
+    }
+}
